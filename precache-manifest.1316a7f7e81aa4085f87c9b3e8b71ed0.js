0eab6e1121a4805e3dbeb166d04b0856d54f2d58
hoseindig <hosein_dig@yahoo.com>
2021-06-19 11:38:49 +0430

    'first'

---
+++ precache-manifest.1316a7f7e81aa4085f87c9b3e8b71ed0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4445fd55a4362758c99f",
+    "url": "/css/app.a016b41b.css"
+  },
+  {
+    "revision": "e56302f8e5475b262f98",
+    "url": "/css/chunk-vendors.a318f5fa.css"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "ceb313c98a7ec60f767d10a92d806f0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f58f3f9196bcee7ea7",
+    "url": "/js/about.3a37da25.js"
+  },
+  {
+    "revision": "4445fd55a4362758c99f",
+    "url": "/js/app.29018a7a.js"
+  },
+  {
+    "revision": "e56302f8e5475b262f98",
+    "url": "/js/chunk-vendors.8b743956.js"
+  },
+  {
+    "revision": "b068e73c7161f903526252aba2eb282a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
